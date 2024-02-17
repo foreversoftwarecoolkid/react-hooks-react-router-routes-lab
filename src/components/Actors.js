1db5c9fb0cd72f69531efff5ba564e6ec227ec3f
Henry <henry.bogita@student.moringaschool.com>
2024-02-17 15:18:47 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { actors } from "../data";
+import { actors, movies } from "../data";
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
+const Actors = () => {
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {movies
+              .filter((movie) => movie.stars.includes(actor.name))
+              .map((movie, index) => (
+                <li key={index}>{movie.title}</li>
+              ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { directors } from "../data";
+import { directors, movies } from "../data";
 
-function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+const Directors = () => {
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <div key={index}>
+          <h2>{director.name}</h2>
+          <ul>
+            {movies
+              .filter((movie) => movie.directorId === director.id)
+              .map((movie, index) => (
+                <li key={index}>{movie.title}</li>
+              ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Directors;
